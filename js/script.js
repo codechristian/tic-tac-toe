9f08129d1e615f6106dec3e0aff70623cb7657e2
codechristian <christianr@ymail.com>
2017-11-24 17:20:54 -0800

    add javascript function to let the computer put an image into random div after user takes a turn

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
 const xImage = 'img/x.png'
 const yImage = 'img/y.png'
+const boxes = document.querySelectorAll('#game-grid div');
+const emptyBoxes = [];
 
-function markWithX(event) {
+for(const box of boxes) {
+  box.addEventListener('click', yourTurn);
+  emptyBoxes.push(box);
+}
+
+function yourTurn(event) {
   const container = event.currentTarget;
   const image = document.createElement('img');
   image.src = xImage;
   container.appendChild(image);
-  container.removeEventListener('click', markWithX);
+  container.removeEventListener('click', yourTurn);
+
+  //remove container box that is no longer empty from emptyBoxes arrray.
+  const indexToRemove = emptyBoxes.indexOf(container);
+  emptyBoxes.splice(indexToRemove, 1);
+
+  artificialIntelligenceTurn();
 }
 
-const boxes = document.querySelectorAll('#game-grid div');
+function artificialIntelligenceTurn() {
+  const index = Math.floor(Math.random() * emptyBoxes.length);
+  const freeSpace = emptyBoxes[index];
 
-for(const box of boxes) {
-  box.addEventListener('click', markWithX);
+  //remove random index box that is no longer empty from emptyBoxes array.
+  emptyBoxes.splice(index, 1);
+  
+  const image = document.createElement('img');
+  image.src = yImage;
+  freeSpace.removeEventListener('click', yourTurn);
+  freeSpace.appendChild(image);
 }
